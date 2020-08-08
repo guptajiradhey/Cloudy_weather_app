0327cb8e6a3c97b3ebe15cfd435419157dee6dec
guptaji <radheys687@gmail.com>
2020-08-08 15:46:47 +0530

    final commit

--- app/src/main/java/com/example/cloudy/splashscreen.java
+++ app/src/main/java/com/example/cloudy/splashscreen.java
@@ -1,10 +1,10 @@
 package com.example.cloudy;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splashscreen extends AppCompatActivity {
 
